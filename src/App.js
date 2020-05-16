f4392384259f580c7b3f8ae04317087b97449edc
Rodrigo <obrido@gmail.com>
2020-05-16 00:31:36 -0400

    Front-end skeleton implemented. Particles implemented.

--- src/App.js
+++ src/App.js
@@ -3,12 +3,29 @@ import './App.css';
 import Navigation from '../src/components/navigation/Navigation'
 import Logo from '../src/components/logo/Logo'
 import Imageform from '../src/components/imageform/Imageform'
-import Rank from './components/'
+import Rank from './components/rank/Rank'
+import Particles from 'react-particles-js'
 
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 20,
+      density: {
+        enable: true,
+        value_area: 800,
+        factor: 200
+        }
+      }
+    }
+  }
 
 function App() {
   return (
     <div className="App">
+{/* Add bellow is the Particles for the background, inside main div, but before the components per se */}
+           <Particles className="particles"
+              params={particlesOptions}
+            />
      <Navigation/>
      <Logo/>
      <Rank/>
--- src/components/rank/Rank.js
+++ src/components/rank/Rank.js
@@ -3,7 +3,12 @@ import React from 'react'
 const Rank = () => {
   return (
     <div>
-      
+      <div className='white f3'>
+        {'Your currnt rank is'}
+      </div>
+      <div>
+        {'#5'}
+      </div>
     </div>
   )
 }
